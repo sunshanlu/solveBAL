66d8a06df428c63f858ff82ce957d7591363c3ad
RookieRoad <ssl2001@126.com>
2023-08-11 12:58:08 +0800

    delete test.txt

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/lu/solveBAL
+# Install script for directory: /home/lu/Project/solveBAL
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/lu/solveBAL/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/lu/Project/solveBAL/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
